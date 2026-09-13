02bc8a5f4c02ca277ed43ba32f86516d57e14569
Ethan <ek258@njit.edu>
2026-09-13 16:42:25 -0400

    created calculator

---
+++ app/calculator/__init__.py
@@ -0,0 +1,31 @@
+from app.operations import addition, subtraction, multiplication, division
+def calculator():
+    print("Welcome to the REPL calculator. To quit, type 'stop'.")
+    while True:
+        user_input = input("Enter an operation (e.g., 'add 2 3'), enter stop to quit: ")
+        if user_input.lower() == 'stop':
+            print("Stoping the calculator")
+            break
+
+        try:
+            operation, num1, num2 = user_input.split()
+            num1, num2 = float(num1), float(num2)
+
+            if operation == 'add':
+                result = addition(num1, num2)
+            elif operation == 'subtract':
+                result = subtraction(num1, num2)
+            elif operation == 'multiply':
+                result = multiplication(num1, num2)
+            elif operation == 'divide':
+                result = division(num1, num2)
+            else:
+                print("Only 'add', 'subtract', 'multiply', or 'divide' are accepted.")
+                continue
+
+            print(f"The result is: {result}")
+
+        except ValueError:
+            print("Invalid input. Enter two numbers and an operation.")
+        except ZeroDivisionError:
+            print("Error: You cannot divide by zero.")
---
+++ tests/test_operation.py
@@ -0,0 +1,18 @@
+
+import pytest
+
+from app.operations import addition, subtraction, multiplication, division
+def test_addition():
+    assert addition(2, 3) == 5
+def test_subtraction(): 
+    assert subtraction(5, 3) == 2
+def test_multiplication():
+    assert multiplication(2, 3) == 6
+def test_division():
+    assert division(6, 3) == 2
+    with pytest.raises(ZeroDivisionError):
+        division(6, 0)
+
+    
+    
+  
